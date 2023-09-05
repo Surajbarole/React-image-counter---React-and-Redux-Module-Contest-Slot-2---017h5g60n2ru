eda2b0ab7f1a807919e0ba905c1cb377f832058d
Suraj Barole <103420352+Surajbarole@users.noreply.github.com>
2023-09-05 19:04:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import '../styles/App.css';
-import star from '../star.png'
+import { Loader } from './Loader';
+import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  function StartCount(){
-    const [size ,setSize]=useState(302);
-    const increase=()=>{
-      setSize(size+2)
+  const [photoData, setPhotoData] = useState(null);
+  const [inputNumber, setInputNumber] = useState('');
+  const [loading, setLoading] = useState(false);
+
+   const handleInputChange = (e) => {
+    setInputNumber(e.target.value);
+  };
+   const fetchPhotoData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${inputNumber}`);
+      const data = await response.json();
+      setPhotoData(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching photo data:', error);
+      setLoading(false);
     }
-  }
-  {  return (
-    <div id="main">
-      <img src={star}  onClick={increase} height="300px" width="300px" />
+  };
+
+   useEffect(() => {
+    if (inputNumber && !isNaN(inputNumber) && inputNumber >= 1 && inputNumber <= 5000) {
+      fetchPhotoData();
+    } else {
+      setPhotoData(null);
+    }
+  }, [inputNumber]);
+    return (
+    <div className="App">
+      <h1>Photo Viewer</h1>
+      <input
+        type="number"
+        value={inputNumber}
+        onChange={handleInputChange}
+      />
+      {loading ? (
+        <Loader />
+      ) : (
+        photoData && (
+          <PhotoFrame url={photoData.url} title={photoData.title} />
+        )
+      )}
     </div>
-  )
-}
-
-
+  );
+};
 export default App;
